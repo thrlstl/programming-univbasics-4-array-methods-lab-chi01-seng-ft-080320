14e253ee0ff59feb6f7f90d5fcca6943a0b7c62c
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-26 20:35:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,6 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element1 = "wow"
-  element2 = "Tardis"
-  array.include?(element1)
-  array.include?(element2)
+  array.include?(element)
 end
 
 def using_sort(array)
